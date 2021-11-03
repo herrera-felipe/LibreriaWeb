a10c0c2335710c43430443581d026e6dfb49c3b5
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-03 11:43:28 -0300

    First commit

---
+++ src/main/java/com/domain/libreria/LibreriaWebApplication.java
@@ -0,0 +1,13 @@
+package com.domain.libreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domain/libreria/LibreriaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
