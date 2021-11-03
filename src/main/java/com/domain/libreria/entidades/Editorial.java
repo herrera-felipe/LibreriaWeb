d17224c7f80b51da0391839a3f6a0b639591fe0b
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-03 13:07:18 -0300

    Terminada entidad Editorial

--- src/main/java/com/domain/libreria/entidades/Editorial.java
+++ src/main/java/com/domain/libreria/entidades/Editorial.java
@@ -17,5 +17,24 @@ public class Editorial {
 	private String nombre;
 	private Boolean alta;
 	
-
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Boolean getAlta() {
+		return alta;
+	}
+	public void setAlta(Boolean alta) {
+		this.alta = alta;
+	}
+	
 }
