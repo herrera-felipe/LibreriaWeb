12daf651162f84377c8ee448d0841fe27b8f3ac0
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-18 13:09:28 -0300

    Modificado LibroServicio
    Agregados metodos para buscar, y metodo modificar

--- src/main/java/com/domain/libreria/servicios/LibroServicio.java
+++ src/main/java/com/domain/libreria/servicios/LibroServicio.java
@@ -1,11 +1,12 @@
 package com.domain.libreria.servicios;
 
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.domain.libreria.entidades.Autor;
 import com.domain.libreria.entidades.Editorial;
@@ -15,50 +16,49 @@ import com.domain.libreria.repositorios.AutorRepositorio;
 import com.domain.libreria.repositorios.EditorialRepositorio;
 import com.domain.libreria.repositorios.LibroRepositorio;
 
-
 @Service
 public class LibroServicio {
 
 	@Autowired
 	private LibroRepositorio libroRepositorio;
-	
+
 	@Autowired
 	private AutorRepositorio autorRespositorio;
-	
+
 	@Autowired
 	private AutorServicio autorServicio;
-	
+
 	@Autowired
 	private EditorialRepositorio editorialRepositorio;
-	
+
 	@Autowired
 	private EditorialServicio editorialServicio;
-	
-	
-	public void guardarLibro(Long isbn, String titulo, Integer anio, Integer ejemplares, String nombreAutor, String nombreEditorial) throws ErrorServicio {
-	
+
+	@Transactional
+	public void guardarLibro(Long isbn, String titulo, Integer anio, Integer ejemplares, String nombreAutor,
+			String nombreEditorial) throws ErrorServicio {
+
 		// Validamos los datos del formulario
 		validar(isbn, titulo, anio, ejemplares, nombreAutor, nombreEditorial);
-		
+
 		// Buscamos el autor en la BD
-		Autor autor = autorRespositorio.buscarAutorPorNombre(nombreAutor); 
-		
+		Autor autor = autorRespositorio.buscarAutorPorNombre(nombreAutor);
+
 		// Sino existe el Autor, lo creamos
 		if (Objects.isNull(autor)) {
-			autor = autorServicio.guardarAutor(nombreAutor); 
+			autor = autorServicio.guardarAutor(nombreAutor);
 		}
-		
+
 		// Buscamos la Editorial
 		Editorial editorial = editorialRepositorio.buscarEditorialPorNombre(nombreEditorial);
-		
+
 		// Sino existe la Editorial, la creamos
 		if (Objects.isNull(editorial)) {
 			editorial = editorialServicio.guardarEditorial(nombreEditorial);
 		}
-		
-		
+
 		Libro libro = new Libro(); // Intanciamos la Entidad
-		
+
 		// Seteamos los valores
 		libro.setIsbn(isbn);
 		libro.setTitulo(titulo);
@@ -67,73 +67,138 @@ public class LibroServicio {
 		libro.setAutor(autor);
 		libro.setEditorial(editorial);
 		libro.setAlta(true);
-		
+
 		// Persistimos en la BD
 		libroRepositorio.save(libro);
 	}
-	
-	public void modificarLibro(String titulo, Integer anio, Integer ejemplares, String nombreAutor, String nombreEditorial) {
-		
-	}
-	
-	public List<Libro> listarLibros(){
-		return null;
-	}
-	
-	public void buscarLibro() {
 
+	@Transactional
+	public void modificarLibro(String id, Long isbn, String titulo, Integer anio, Integer ejemplares,
+			String nombreAutor, String nombreEditorial) throws ErrorServicio {
+
+		Libro libro = libroRepositorio.findById(id).get(); // Buscamos el libro a modificar
+
+		validar(isbn, titulo, anio, ejemplares, nombreAutor, nombreEditorial); // Validamos los datos del formulario
+
+		Autor autor = autorRespositorio.buscarAutorPorNombre(nombreAutor); // Buscamos el autor en la BD
+
+		// Sino existe el Autor, lo creamos
+		if (Objects.isNull(autor)) {
+			autor = autorServicio.guardarAutor(nombreAutor);
+		}
+
+		Editorial editorial = editorialRepositorio.buscarEditorialPorNombre(nombreEditorial); // Buscamos la Editorial
+
+		// Sino existe la Editorial, la creamos
+		if (Objects.isNull(editorial)) {
+			editorial = editorialServicio.guardarEditorial(nombreEditorial);
+		}
+
+		// Seteamos los valores
+		libro.setIsbn(isbn);
+		libro.setTitulo(titulo);
+		libro.setAnio(anio);
+		libro.setEjemplares(ejemplares);
+		libro.setAutor(autor);
+		libro.setEditorial(editorial);
 		
+		libroRepositorio.save(libro); // persistimos en la BD
 	}
-	
-	
+
+	@Transactional(readOnly = true)
+	public List<Libro> listarLibrosActivos() {
+		return libroRepositorio.buscarActivos();
+	}
+
+	@Transactional(readOnly = true)
+	public List<Libro> listarTodos() {
+		return libroRepositorio.findAll();
+	}
+
+	@Transactional(readOnly = true)
+	public Libro buscarPorIsbn(String id) {
+
+		Libro entidad = libroRepositorio.getById(id);
+
+		return entidad;
+	}
+
+	@Transactional(readOnly = true)
+	public List<Libro> buscarPorTitulo(String titulo) {
+
+		List<Libro> libros = listarTodos();
+
+		List<Libro> encontrados = new ArrayList<>();
+
+		// Recorremos la lista
+		for (Libro libro : libros) {
+
+			String aux = libro.getTitulo();
+
+			if (aux.contains(titulo) || aux.equalsIgnoreCase(titulo)) {
+				encontrados.add(libro);
+			}
+		}
+
+		// Validamos si se encontro algun resultado o no
+		if (encontrados.isEmpty() || encontrados == null) {
+			return null;
+		}
+
+		return encontrados;
+	}
+
+	@Transactional
 	public Libro alta(String id) {
-		
+
 		Libro entidad = libroRepositorio.getById(id);
-		
+
 		entidad.setAlta(true);
-		
+
 		return libroRepositorio.save(entidad);
 	}
-	
+
+	@Transactional
 	public Libro baja(String id) {
-		
+
 		Libro entidad = libroRepositorio.getById(id);
-		
+
 		entidad.setAlta(false);
-		
+
 		return libroRepositorio.save(entidad);
 	}
-	
+
 	/*
 	 * Valida que los campos del formulario no sean nullos
 	 */
-	public void validar(Long isbn, String titulo, Integer anio, Integer ejemplares, String autor, String editorial) throws ErrorServicio {
-		
-		if (isbn == null || isbn <=0 || isbn.toString().isEmpty()) {
+	public void validar(Long isbn, String titulo, Integer anio, Integer ejemplares, String autor, String editorial)
+			throws ErrorServicio {
+
+		if (isbn == null || isbn <= 0 || isbn.toString().isEmpty()) {
 			throw new ErrorServicio("El campo Isbn no puede estar vacio, ni tener el valor de cero");
 		}
-		
+
 		if (titulo == null || titulo.isEmpty()) {
 			throw new ErrorServicio("El campo Titulo no puede estar vacio.");
 		}
-		
+
 		if (anio == null || anio <= 0 || anio.toString().isEmpty()) {
 			throw new ErrorServicio("El campo Año no puede estar vacio.");
 		}
-		
+
 		if (ejemplares == null || ejemplares <= 0 || ejemplares.toString().isEmpty()) {
 			throw new ErrorServicio("El campo Ejemplares no puede estar vacio.");
 		}
-		
+
 		if (autor == null || autor.isEmpty()) {
 			throw new ErrorServicio("El campo Autor no puede estar vacio.");
 		} else {
-			 
+
 		}
-		
+
 		if (editorial == null || editorial.isEmpty()) {
 			throw new ErrorServicio("El campo Editorial no puede estar vacio.");
 		}
 	}
-	
+
 }
