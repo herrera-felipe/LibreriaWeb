f21cbccc8d15ac83dde9d12401c58950b3ae551a
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-19 11:19:03 -0300

    modificado AutorServicio
    Agregados metodos para modificar, alta, baja, listar y buscar

--- src/main/java/com/domain/libreria/servicios/AutorServicio.java
+++ src/main/java/com/domain/libreria/servicios/AutorServicio.java
@@ -1,7 +1,12 @@
 package com.domain.libreria.servicios;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.domain.libreria.entidades.Autor;
 import com.domain.libreria.errores.ErrorServicio;
@@ -12,25 +17,119 @@ public class AutorServicio {
 
 	@Autowired
 	private AutorRepositorio autorRepositorio;
-	
+
+	@Transactional
 	public Autor guardarAutor(String nombre) throws ErrorServicio {
-		
+
 		validar(nombre);
-		
+
 		Autor autor = new Autor(); // Instanciamos la Entidad
-		
+
 		// Seteamos atributos
 		autor.setNombre(nombre);
-		autor.setAlta(true); 
-		
+		autor.setAlta(true);
+
 		// Persistimos con el repositorio
 		return autorRepositorio.save(autor);
 	}
-	
+
+	@Transactional
+	public void modificarAutor(String id, String nombre) throws ErrorServicio {
+
+		validar(nombre);
+
+		Optional<Autor> respuesta = autorRepositorio.findById(id);
+
+		if (respuesta.isPresent()) {
+			Autor autor = respuesta.get();
+			autor.setNombre(nombre);
+			autorRepositorio.save(autor);
+		} else {
+			throw new ErrorServicio("No se encontró el autor solicitado");
+		}
+	}
+
+	@Transactional
+	public void alta(String id) throws ErrorServicio {
+
+		Optional<Autor> respuesta = autorRepositorio.findById(id);
+
+		if (respuesta.isPresent()) {
+			Autor autor = respuesta.get();
+			autor.setAlta(true);
+			autorRepositorio.save(autor);
+		} else {
+			throw new ErrorServicio("No se encontró el autor solicitado");
+		}
+	}
+
+	@Transactional
+	public void baja(String id) throws ErrorServicio {
+
+		Optional<Autor> respuesta = autorRepositorio.findById(id);
+
+		if (respuesta.isPresent()) {
+			Autor autor = respuesta.get();
+			autor.setAlta(false);
+			autorRepositorio.save(autor);
+		} else {
+			throw new ErrorServicio("No se encontró el autor solicitado");
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public List<Autor> listarTodos() {
+		return autorRepositorio.findAll();
+	}
+
+	@Transactional(readOnly = true)
+	public List<Autor> listarActivos() {
+		return autorRepositorio.buscarActivos();
+	}
+
+	@Transactional(readOnly = true)
+	public List<Autor> buscarPorNombre(String nombre) {
+
+		List<Autor> autores = listarTodos(); // Traemos todos los autores
+
+		List<Autor> encontrados = new ArrayList<>();
+
+		// Recorremos la lista
+		for (Autor autor : autores) {
+			String aux = autor.getNombre();
+
+			if (aux.contains(nombre) || aux.equalsIgnoreCase(nombre)) {
+				encontrados.add(autor);
+			}
+		}
+
+		if (encontrados.isEmpty() || encontrados == null) {
+			return null;
+		}
+
+		return encontrados;
+	}
+
+	@Transactional(readOnly = true)
+	public Autor buscarPorId(String id) {
+
+		Optional<Autor> respuesta = autorRepositorio.findById(id);
+
+		Autor autor = new Autor();
+
+		if (respuesta.isPresent()) {
+			autor = respuesta.get();
+		} else {
+			return null;
+		}
+
+		return autor;
+	}
+
 	public void validar(String nombre) throws ErrorServicio {
 		if (nombre == null || nombre.isEmpty()) {
 			throw new ErrorServicio("El nombre no puede estar vacio.");
 		}
 	}
-	
+
 }
