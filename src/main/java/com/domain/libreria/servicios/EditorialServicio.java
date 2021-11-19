9858837f6a499c9b67d753978f07fde9da25aab7
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-19 11:57:50 -0300

    Modificado Servicio Editorial
    Agregados los metodos para: *Modificar *Listar *Alta *Baja

--- src/main/java/com/domain/libreria/servicios/EditorialServicio.java
+++ src/main/java/com/domain/libreria/servicios/EditorialServicio.java
@@ -1,7 +1,12 @@
 package com.domain.libreria.servicios;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.domain.libreria.entidades.Editorial;
 import com.domain.libreria.errores.ErrorServicio;
@@ -13,9 +18,7 @@ public class EditorialServicio {
 	@Autowired
 	private EditorialRepositorio editorialRepositorio;
 
-	/*
-	 * 
-	 */
+	@Transactional
 	public Editorial guardarEditorial(String nombre) throws ErrorServicio {
 
 		validar(nombre);
@@ -30,6 +33,82 @@ public class EditorialServicio {
 		return editorialRepositorio.save(editorial);
 	}
 
+	@Transactional
+	public void modificarEditorial(String id, String nombre) throws ErrorServicio {
+
+		validar(nombre);
+
+		Optional<Editorial> respuesta = editorialRepositorio.findById(id); // Buscamos la editorial
+
+		if (respuesta.isPresent()) {
+			Editorial editorial = respuesta.get(); // Si trae resultado la agregamos a la entidad
+			editorial.setNombre(nombre); // Seteamos el nuevo valor
+			editorialRepositorio.save(editorial); // Persistimos en la BD
+		} else {
+			throw new ErrorServicio("No se encontró la editorial solicitada");
+		}
+	}
+
+	@Transactional
+	public void alta(String id) throws ErrorServicio {
+
+		Optional<Editorial> respuesta = editorialRepositorio.findById(id); // Buscamos la editorial
+
+		if (respuesta.isPresent()) {
+			Editorial editorial = respuesta.get(); // Si trae resultado la agregamos a la entidad
+			editorial.setAlta(true); // Damos alta
+			editorialRepositorio.save(editorial); // Persistimos en la BD
+		} else {
+			throw new ErrorServicio("No se encontró la editorial solicitada");
+		}
+	}
+	
+	@Transactional
+	public void baja(String id) throws ErrorServicio {
+
+		Optional<Editorial> respuesta = editorialRepositorio.findById(id); // Buscamos la editorial
+
+		if (respuesta.isPresent()) {
+			Editorial editorial = respuesta.get(); // Si trae resultado la agregamos a la entidad
+			editorial.setAlta(false); // damos la baja
+			editorialRepositorio.save(editorial); // Persistimos en la BD
+		} else {
+			throw new ErrorServicio("No se encontró la editorial solicitada");
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public List<Editorial> listarTodos() {
+		return editorialRepositorio.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Editorial> listarActivos() {
+		return editorialRepositorio.buscarActivos();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Editorial> buscarPorNombre(String nombre) {
+		
+		List<Editorial> listaEditorial = listarTodos(); //traemos todas las editoriales
+		
+		List<Editorial> encontrados = new ArrayList<>();
+		
+		// Recorremos la lista
+		for (Editorial editorial : listaEditorial) {
+			String aux = editorial.getNombre();
+			
+			if (aux.contains(nombre) || aux.equalsIgnoreCase(nombre)) {
+				encontrados.add(editorial);
+			}
+		}
+		
+		if (encontrados.isEmpty() || encontrados == null) {
+			return null;
+		}
+		
+		return encontrados;
+	}
 	
 	public void validar(String nombre) throws ErrorServicio {
 		if (nombre == null || nombre.isEmpty()) {
