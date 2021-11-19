6309ed398c5b4ccbe1c75a7e4530092766dfa061
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-19 11:54:43 -0300

    Modificado EditorialRepositorio
    Agregada consulta por alta

--- src/main/java/com/domain/libreria/repositorios/EditorialRepositorio.java
+++ src/main/java/com/domain/libreria/repositorios/EditorialRepositorio.java
@@ -1,5 +1,7 @@
 package com.domain.libreria.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,10 @@ public interface EditorialRepositorio extends JpaRepository<Editorial, String> {
 
 	@Query("SELECT e FROM Editorial e WHERE e.nombre = :nombre")
 	public Editorial buscarEditorialPorNombre(@Param("nombre") String nombre);
+	
+	@Query("SELECT e FROM Editorial e WHERE e.nombre = :nombre")
+	public List<Editorial> listarEditorial(@Param("nombre") String nombre);
+	
+	@Query("SELECT e FROM Editorial e WHERE e.alta = true")
+	public List<Editorial> buscarActivos();
 }
