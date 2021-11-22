761820a9566cb59b720deab6afc396b09e8950e9
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-22 15:49:33 -0300

    Separada la funcionalidad de los controladores
    Ahora el controlador inicio solo se encarga del index.html

--- src/main/java/com/domain/libreria/controladores/ControladorInicio.java
+++ src/main/java/com/domain/libreria/controladores/ControladorInicio.java
@@ -2,9 +2,7 @@ package com.domain.libreria.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -15,14 +13,4 @@ public class ControladorInicio {
         return "index";
     }
     
-
-    @GetMapping("/autor")
-    public String resgistroAutor() {
-        return "autorform";
-    }
-
-    @GetMapping("/editorial")
-    public String resgistroEditorial() {
-        return "editorialform";
-    }
 }
