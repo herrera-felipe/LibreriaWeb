fcb15c63c25cb276f5092641dbf9377ac72f48b3
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-22 14:03:35 -0300

    Separada funcionalidad
    Los metodos GetMapping y PostMapping del libro, movidos a su propio controlador

--- src/main/java/com/domain/libreria/controladores/ControladorInicio.java
+++ src/main/java/com/domain/libreria/controladores/ControladorInicio.java
@@ -2,13 +2,27 @@ package com.domain.libreria.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class ControladorInicio {
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    
 
+    @GetMapping("/autor")
+    public String resgistroAutor() {
+        return "autorform";
+    }
+
+    @GetMapping("/editorial")
+    public String resgistroEditorial() {
+        return "editorialform";
+    }
 }
