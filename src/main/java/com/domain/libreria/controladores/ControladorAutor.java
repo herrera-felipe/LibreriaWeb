f8c8d9e3aa8554cdf1d9ab544085f495933a6683
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-23 21:15:07 -0300

    Terminado controladorAutor
    Agregada la funcionalidad para listar, alta, baja y modificacion. Inyectando en el html los resultados de las peticiones

--- src/main/java/com/domain/libreria/controladores/ControladorAutor.java
+++ src/main/java/com/domain/libreria/controladores/ControladorAutor.java
@@ -1,13 +1,17 @@
 package com.domain.libreria.controladores;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.domain.libreria.entidades.Autor;
 import com.domain.libreria.servicios.AutorServicio;
 
 @Controller
@@ -38,6 +42,70 @@ public class ControladorAutor {
 			
 			modelo.put("error", "Fallo el registro");
 			return "autorform";
+		}
+	}
+	
+	@GetMapping("/lista")
+	public String lista(ModelMap modelo) {
+		
+		try {
+			//Traemos la lista de autores
+			List<Autor> autoresLista = autorServicio.listarTodos();
+			
+			modelo.addAttribute("autores", autoresLista); // la var "autores" lleva la ista al html
+			return "lista-autor";
+			
+		} catch (Exception e) {
+			return "lista-autor";
+		}
+		
+	}
+	
+	@GetMapping("/baja/{id}")
+	public String baja(@PathVariable String id) {
+		
+		try {
+			autorServicio.baja(id);
+			return "redirect:/autor/lista";
+			
+		} catch (Exception e) {
+			return "redirect:/autor/lista";
+		}
+	}
+	
+	@GetMapping("/alta/{id}")
+	public String alta(@PathVariable String id) {
+		
+		try {
+			autorServicio.alta(id);
+			return "redirect:/autor/lista";
+			
+		} catch (Exception e) {
+			return "redirect:/autor/lista";
+		}
+	}
+	
+	@GetMapping("/modificar/{id}") // {id} es una PathVariable
+	public String modificar(@PathVariable String id, ModelMap modelo) {
+		
+		modelo.put("autor", autorServicio.buscarPorId(id)); // Inyectamos al html el autor al que corresponde el id
+		
+		return "autor-form-modificar";
+	}
+	
+	@PostMapping("/modificar/{id}")
+	public String modificar(@PathVariable String id, @RequestParam String nombre, ModelMap modelo) {
+		
+		try {
+			autorServicio.modificarAutor(id, nombre);
+			
+			modelo.put("exito", "Modificación Exitosa");
+			return "redirect:/autor/lista";
+			
+		} catch (Exception e) {
+			
+			modelo.put("error", "No se realizo la modificación");
+			return "autor-form-modificar";
 		}
 	}
 }
